75a86fd23187e50aa2bc40f295e3409af10241d1
Peerapatllo <45157172+Peerapatllo@users.noreply.github.com>
2023-07-13 02:38:08 +0700

    Update Lowcpu.lua

--- Lowcpu.lua
+++ Lowcpu.lua
@@ -1,6 +1,7 @@
 repeat wait() until game:IsLoaded()
 local RunService = game:GetService("RunService")
 RunService:Set3dRenderingEnabled(false)
+setfpscap(60)
 local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
 local g = game
 local w = g.Workspace
