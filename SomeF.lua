b356fa2327a97339294c896d8f4b5f9d1ee2d8b1
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2023-07-17 13:56:58 +0700

    Update SomeF.lua

--- SomeF.lua
+++ SomeF.lua
@@ -309,7 +309,7 @@ function webhook()
           ["thumbnail"] = {
             ['url'] = thumbnails_avatar.data[1].imageUrl,
             },
-            ["description"] = "# <a:load:938430201984065577>__กำลังฟาร์มอยู่จร้า__<a:load:938430201984065577> \n<a:d4:1113801645931896912> **Name: **||"..game:GetService("Players").LocalPlayer.Name.." ("..game:GetService("Players").LocalPlayer.DisplayName..")".."|| <a:d5:1113801649014718545> \n<a:Dot:923905792162275368> "..lv.." <a:mee6lvlup:1123179161054355527> \n<a:Dot:923905792162275368> Gem "..ttgems.." <:Gems:1118956171995381850> \n<a:Dot:923905792162275368> เพชรที่ฟาร์ม: **".. Settings.GemFramed .. " / " .. Settings.GemtoFarm .."** <:Gems:1118956171995381850>",
+            ["description"] = "# <a:load:938430201984065577>__กำลังฟาร์มอยู่จร้า__<a:load:938430201984065577> \n<a:d4:1113801645931896912> **Name: **||"..game:GetService("Players").LocalPlayer.Name.." ("..game:GetService("Players").LocalPlayer.DisplayName..")".."|| <a:d5:1113801649014718545> \n<a:Dot:923905792162275368> "..lv.." <a:mee6lvlup:1123179161054355527> \n<a:Dot:923905792162275368> Gem "..ttgems.." <:Gems:1118956171995381850> \n<a:Dot:923905792162275368> สถานะฟาร์ม: **".. Settings.GemFramed .. " / " .. Settings.GemtoFarm .."** <:Gems:1118956171995381850>",
           ["author"] = {
                 ["name"] = "INDYBUX Service",
                 ["icon_url"] = "https://img.pic.in.th/oie_27168246JMfoaRb.gif"
