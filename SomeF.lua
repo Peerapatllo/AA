73717df1b39e58ee70c3527c41eb744a32711596
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2023-07-19 17:53:31 +0700

    Update SomeF.lua

--- SomeF.lua
+++ SomeF.lua
@@ -731,6 +731,10 @@ local function CheckGemAll()
 	if tonumber(Settings.GemFramed) >= tonumber(Settings.GemtoFarm) and tonumber(Settings.GemtoFarm) ~= 0 then
 		if not Settings.KiwwyKick then
             GemsWebhook()
+            Settings.autostart = false
+			Settings.KiwwyKick = true
+			saveSettings()
+			game.Players.LocalPlayer:Kick("Farm Success | INDY BUX")
             delfile("V2_Anime_Adventures/" .. game.Players.LocalPlayer.Name .. "_AnimeAdventures.json")
             game:GetService("TeleportService"):Teleport(8304191830, game.Players.LocalPlayer)
 		end
