b4dd76b989c99f0b124b76481d87dbdeda5c63cc
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2023-08-02 19:08:41 +0700

    Update SomeSE.lua

--- SomeSE.lua
+++ SomeSE.lua
@@ -2968,10 +2968,10 @@ local function LowCPUModeT()
     task.spawn(function()
         while task.wait() do
             if isrbxactive() ~= true and Settings.lowCpuMode then
-                setfpscap(30)
+                setfpscap(5)
                 game:GetService("RunService"):Set3dRenderingEnabled(false)
             else
-                setfpscap(1111)
+                setfpscap(5)
                 game:GetService("RunService"):Set3dRenderingEnabled(true)
             end
         end
