5889ead813080c25d8326f082d3e2b0c75bfc19f
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2023-08-16 20:21:52 +0700

    Update SomeDSE.lua

--- SomeDSE.lua
+++ SomeDSE.lua
@@ -1,6 +1,6 @@
 --updatefix
 local version = "v2.0.0b31"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Peerapatllo/AA/main/Lowcpu.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Peerapatllo/AA/main/Lowcpu.lua"))()
 ---// Loading Section \\---
 repeat  task.wait() until game:IsLoaded()
 if game.PlaceId == 8304191830 then
