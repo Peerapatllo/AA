39812e4048e21a43a6012e369bf07ca216653c07
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2023-08-17 17:50:43 +0700

    Update SomeC.lua

--- SomeC.lua
+++ SomeC.lua
@@ -650,6 +650,15 @@ local exec = tostring(identifyexecutor())
 local Window = Uilib.new(true, "                         "..namegame.." ( "..display .." ) 🏯 Infinite Castle: " .. Settings.GemFramed .. " / " .. Settings.GemtoFarm .." 🏯")
 Window.ChangeToggleKey(Enum.KeyCode.C)
 
+local Farmsetup = Window:Category("🧑🏻‍🌾 Custom Farm")
+local Units = Farmsetup:Sector("🧙‍ Select Units")
+local blank1 = Farmsetup:Sector("")
+local StartFarm = Farmsetup:Sector("🧑🏻‍🌾‍ Farming")
+local blank2 = Farmsetup:Sector("")
+local SetGem = Farmsetup:Sector("💎‍ SetGem")
+local blank3 = Farmsetup:Sector("")
+local hook = Farmsetup:Sector("🌐‍ Webhook")
+
 local Farm = Window:Category("🤖 Auto Farm")
 local SelectUnits = Farm:Sector("🧙‍ Select Units")
 local SelectWorld = Farm:Sector("🌏 Select World")
@@ -788,6 +797,59 @@ spawn(function()
 		if show_kicksetting then show_kicksetting.label.Text = "Kick Setting : " .. tostring(Settings.KiwwyKick) end
 	end
 end)
+
+StartFarm:Cheat("Checkbox","👨‍🌾 Auto Place unit  ", function(bool)
+    print(bool)
+    Settings.AutoFarm = bool
+    saveSettings()
+end,{enabled = Settings.AutoFarm })
+StartFarm:Cheat("Checkbox","🏰️ Auto Infinity Castle   ", function(bool)
+    print(bool)
+    Settings.AutoInfinityCastle = bool
+    saveSettings()
+end,{enabled = Settings.AutoInfinityCastle})
+
+Units:Cheat("Button", "🧙 Select Units", function() --Selects Currently Equipped Units!
+    Settings.SelectedUnits = {
+        U1 = "nil",
+        U2 = "nil",
+        U3 = "nil",
+        U4 = "nil",
+        U5 = "nil",
+        U6 = "nil"
+    }
+    saveSettings()
+    GetUnits()
+end)
+
+SetGem:Cheat("Textbox", "เพชรที่ต้องการฟาร์ม", function(Value)
+    if type(tonumber(Value)) == "number" then
+        Show_GemtoFarm.label.Text = "เพชรที่ต้องการฟาร์ม : " .. Value
+        Settings.GemtoFarm = Value
+        saveSettings()
+        changetextgemall()
+    else
+        spawn(function()
+            ChangeErrorCode("กรุณาใส่ตัวเลขเท่านั้น (1)")
+            wait(1)
+            ChangeErrorCode("None")
+        end)
+    end
+end, { placeholder = Settings.GemtoFarm })
+SetGem:Cheat("Button", "Redeem All Code", function()
+    print(Settings.redeemc)
+Reedemcode()
+end)
+SetGem:Cheat("Button", "Leave To Lobby", function()
+    warn("Return to Lobby")
+    Teleport()
+end)
+
+hook:Cheat("Button", "Finish Webhook", function()
+    print(Settings.WebhookUrl)
+    GemsWebhook()
+end)
+
 --[[ THE BEST ]]--
 
 ----------------------------------------------
